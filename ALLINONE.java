9007ea1c8e738a887825429f9058cbfb2892ff32
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-04-15 23:05:29 +0500

    exercise one question

---
+++ ALLINONE.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+public class ALLINONE{
+    public static void main(String[] args){
+        LinkedList<String> list = LinkedList<String>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+
+
+    }
+    system.out.println("The linked list: " + l_list);
+}
