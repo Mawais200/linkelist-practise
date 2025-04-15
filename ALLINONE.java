9f0b23162362c29c80f62fe2132ae722cf6dd8ce
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-04-15 23:28:22 +0500

    exercise 3

--- ALLINONE.java
+++ ALLINONE.java
@@ -1,14 +1,14 @@
 import java.util.LinkedList;
-public class ALLINONE{
-    public static void main(String[] args){
-        LinkedList<String> list = LinkedList<String>();
+
+public class ALLINONE {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
         list.add("A");
         list.add("B");
         list.add("C");
         list.add("D");
         list.add("E");
 
-
+        System.out.println("The linked list: " + list);
     }
-    system.out.println("The linked list: " + l_list);
 }
---
+++ ALLITRATE.java
@@ -0,0 +1,15 @@
+package linkelist-practise;
+import java.util.*;
+
+public class ALLITRATE {
+    LinkedList<String> list = new LinkedList<String>();
+    list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+        for(String element : lisy)
+
+        System.out.println("The linked list: " + element);
+    
+}
