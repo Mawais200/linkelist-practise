f8a55dbd9c3f9d79bf5f1b1d75146afa974652af
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-04-16 08:33:55 +0500

    make it more conscise and clean

--- ALLINONE.java
+++ ALLINONE.java
@@ -9,6 +9,23 @@ public class ALLINONE {
         list.add("D");
         list.add("E");
 
-        System.out.println("The linked list: " + list);
+        System.out.println("Initial linked list: " + list);
+
+        // Add elements at specific positions
+        list.addFirst("Start");
+        list.addLast("End");
+        System.out.println("After adding elements: " + list);
+
+        // Remove elements
+        list.remove("C");
+        list.removeFirst();
+        list.removeLast();
+        System.out.println("After removing elements: " + list);
+
+        // Iterate through the list
+        System.out.println("Iterating through the list:");
+        for (String item : list) {
+            System.out.println(item);
+        }
     }
 }
